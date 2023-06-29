58331da3f3833a28c4893a2d37023aec80759f69
slahmer <seyyidahmed.lahmer@unipd.it>
2023-06-29 10:46:56 +0200

    Final

--- helpers/wandbQuerying.py
+++
@@ -1,9 +0,0 @@
-import wandb
-api = wandb.Api()
-
-# run is specified by <entity>/<project>/<run id>
-run = api.run("slahmer/costoflearning-icc23/b6pk0z9k")
-
-# save the metrics for the run to a csv file
-metrics_dataframe = run.history()
-metrics_dataframe.to_csv("metrics.csv")
